561aef7280984b1eac0b7dd37b9e49719ab0978c
itsmeusama <mohamedusama881@gmail.com>
2022-11-14 15:12:43 +0530

    user-reg_part-done

--- src/com/yb7/pos/controller/LoginFormController.java
+++ src/com/yb7/pos/controller/LoginFormController.java
@@ -1,4 +1,24 @@
 package com.yb7.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane loginFormContext;
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage window = (Stage) loginFormContext.getScene().getWindow();
+        window.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml")))
+        );
+    }
 }
--- src/com/yb7/pos/controller/SignUpFormController.java
+++ src/com/yb7/pos/controller/SignUpFormController.java
@@ -5,6 +5,7 @@ import com.jfoenix.controls.JFXTextField;
 import com.yb7.pos.db.Database;
 import com.yb7.pos.model.User;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 
 public class SignUpFormController {
@@ -19,6 +20,27 @@ public class SignUpFormController {
     }
 
     public void signUpOnAction(ActionEvent actionEvent) {
+
+        String realPwd = txtPassword.getText().trim();
+        String matchPwd = txtRePassword.getText().trim();
+
+        if(!realPwd.equals(matchPwd)){
+            new Alert(Alert.AlertType.WARNING,"Passwords not matching").show();
+            return;
+        }
+
+        User u = new User(txtEmail.getText().trim(),txtFullName.getText(),txtContact.getText(),txtPassword.getText());
+        if(saveUser(u)){
+            new Alert(Alert.AlertType.CONFIRMATION, "User Registered").show();
+            clearFields();
+        }
+        else{
+            new Alert(Alert.AlertType.WARNING,"Try Again").show();
+        }
+    }
+
+    private void clearFields() {
+        txtEmail.clear();txtFullName.clear();txtContact.clear();txtPassword.clear();txtRePassword.clear();
     }
 
     private boolean saveUser(User u){
